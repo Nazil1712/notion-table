8c8efadca5fe746e0cc32c60a03e0afc95827293
Nazil1712 <ndhalwala@gmail.com>
2026-01-02 18:51:44 +0530

    Added dropdown for company, colours and added date etc.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { RevoGrid } from "@revolist/react-datagrid";
 import { users } from "./data";
-import { columns } from "./columns";
+import { columns, columnTypes } from "./columns";
 
 export default function App() {
   return (
     <div className="p-6 h-screen bg-gray-50">
-      <div className="h-125 bg-white rounded-xl shadow">
+      <div className="h-125 bg-white rounded-xl shadow w-220">
         <RevoGrid
           source={users}
           columns={columns}
           rowHeaders
           resize
           filter
+          columnTypes={columnTypes}
           theme="material"
         />
       </div>
--- src/columns.ts
+++ src/columns.ts
@@ -1,9 +1,57 @@
-import type {
-  ColumnRegular,
-  ColumnGrouping,
-} from "@revolist/react-datagrid";
+import type { ColumnRegular, ColumnGrouping } from "@revolist/react-datagrid";
 
 import { NameCell, AgeCell, EyesCell } from "./cells";
+import SelectTypePlugin from "@revolist/revogrid-column-select";
+import Plugin from "@revolist/revogrid-column-date";
+
+const dropdown = [
+  {
+    labelKey: "label",
+    valueKey: "value",
+    source: [
+      { label: "Go Mindz", value: "Go Mindz" },
+      { label: "Zam Zam", value: "Zam Zam" },
+      { label: "TCS", value: "TCS" },
+      { label: "Infosys", value: "Infosys" },
+      { label: "Wipro", value: "Wipro" },
+      { label: "Accenture", value: "Accenture" },
+      { label: "Cognizant", value: "Cognizant" },
+      { label: "HCL", value: "HCL" },
+      { label: "Capgemini", value: "Capgemini" },
+      { label: "IBM", value: "IBM" },
+      { label: "Tech Mahindra", value: "Tech Mahindra" },
+      { label: "Oracle", value: "Oracle" },
+      { label: "Zoho", value: "Zoho" },
+      { label: "Microsoft", value: "Microsoft" },
+      { label: "Flipkart", value: "Flipkart" },
+      { label: "Amazon", value: "Amazon" },
+      { label: "Deloitte", value: "Deloitte" },
+      { label: "Startup Inc", value: "Startup Inc" },
+      { label: "Paytm", value: "Paytm" },
+      { label: "Meta", value: "Meta" },
+      { label: "Swiggy", value: "Swiggy" },
+      { label: "Netflix", value: "Netflix" },
+      { label: "Byjus", value: "Byjus" },
+      { label: "Adobe", value: "Adobe" },
+      { label: "PhonePe", value: "PhonePe" },
+    ],
+  },
+  {
+    labelKey: "label",
+    valueKey: "value",
+    source: [
+      { label: "Blue", value: "blue" },
+      { label: "Green", value: "green" },
+      { label: "Brown", value: "brown" },
+      { label: "Black", value: "black" },
+      { label: "Grey", value: "grey" },
+      { label: "Hazel", value: "hazel" },
+      { label: "Maroon", value: "maroon" },
+      { label: "Dim Grey", value: "dimgrey" },
+    ],
+  },
+];
+
 
 export const columns: (ColumnRegular | ColumnGrouping)[] = [
   {
@@ -14,6 +62,7 @@ export const columns: (ColumnRegular | ColumnGrouping)[] = [
         name: "Name",
         size: 220,
         cellTemplate: NameCell,
+        pin: "colPinStart",
       } as ColumnRegular,
     ],
   },
@@ -27,16 +76,39 @@ export const columns: (ColumnRegular | ColumnGrouping)[] = [
         cellTemplate: AgeCell,
       } as ColumnRegular,
       {
+        ...dropdown[0],
         prop: "company",
         name: "Company",
         size: 150,
-      } as ColumnRegular,
+        columnType: "select",
+      },
       {
+        ...dropdown[1],
         prop: "eyes",
         name: "Eyes",
         size: 120,
         cellTemplate: EyesCell,
+        columnType: "select",
+      },
+      {
+        prop: "birthdate",
+        name: "Birth Date",
+        size: 180,
+        columnType: "date",
+        direction: "left",
+        required: "true",
+        valueAsDate: "true",
+      },
+      {
+        prop: "a",
+        name: "A",
+        size: 120,
       } as ColumnRegular,
     ],
   },
 ];
+
+export const columnTypes = {
+  select: new SelectTypePlugin(),
+  date: new Plugin(),
+};
